54fc2012a848ac486219b1e2dd082a4571f8115f
Abhishek Kumar <Dharmesh.Godha@Advaiya.com>
2023-06-20 08:32:35 +0530

    maximumInwindowsize

---
+++ SlidingWindow/MaximumInWindowSize.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumInWindowSize {
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 3, -1, -3, 5, 3, 6, 7 };
+
+    int k = 3;
+    int i = 0;
+    int max = -999;
+    int[] res = new int[arr.length - 2];
+    Queue<Integer> queue = new LinkedList<>();
+    for (int j = 0; j < arr.length; j++) {
+      if (arr[j] > max) {
+        max = arr[j];
+      }
+      if (queue.isEmpty()) {
+        queue.add(max);
+      }
+      if (!queue.isEmpty() && queue.peek() < max) {
+        queue.clear();
+        queue.add(max);
+      }
+      if (!queue.isEmpty() && queue.peek() >= arr[j]) {
+        queue.add(arr[j]);
+      }
+      if (j >= k - 1) {
+        res[j - k + 1] = queue.peek();
+        if (queue.peek() == arr[i]) {
+          queue.poll();
+        }
+        i++;
+      }
+    }
+    System.out.println(Arrays.toString(res));
+  }
+}
